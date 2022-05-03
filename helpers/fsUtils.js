9594a657d8e123f5a4fad5f9ef7da833e4f2a7ac
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-03 16:14:16 +0800

    Update server.js and note.js functionality

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -27,8 +27,29 @@ const readAndAppend = (content, file) => {
       const parsedData = JSON.parse(data);
       parsedData.push(content);
       writeToFile(file, parsedData);
+      
     }
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {string} id The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+ const readAndDelete = (id, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        var obj=parsedData.find(item=>item.id==id);
+        parsedData.remove(obj);
+        writeToFile(file, parsedData);
+        return parsedData;
+      }
+    });
+  };
+
+module.exports = { readFromFile, writeToFile, readAndAppend,readAndDelete };
---
+++ routes/note.js
@@ -0,0 +1,42 @@
+const epr = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile,readAndDelete } = require('../helpers/fsUtils');
+
+
+// GET Route for retrieving all the note
+epr.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+epr.delete('/:id', (req, res) =>
+readAndDelete(req.params.id,'./db/db.json').then((data) => res.json(data))
+);
+
+// POST Route for submitting note
+epr.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text ) {
+      // Variable for the object we will save
+      const newnote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newnote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newnote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+  
+  module.exports = epr;
