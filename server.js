a80622c714a970b385856ca570097faf378f7f9b
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-03 21:37:43 +0800

    connect to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3002;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
@@ -30,6 +30,4 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
